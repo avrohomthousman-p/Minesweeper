528a6ef8fb6ee1bc6834c161fc39ffa3992ad673
avrohomthousman-p <77360734+avrohomthousman-p@users.noreply.github.com>
2022-01-23 13:04:59 -0500

    Update IModel.cs

--- IModel.cs
+++ IModel.cs
@@ -4,12 +4,8 @@ using System.Drawing;
 
 namespace MinesweeperModel
 {
-    // Reference Game: https://www.google.com/search?q=minesweeper&rlz=1C1EJFC_enUS867US867&oq=mine&aqs=chrome.0.69i59j46i67i433j46i67j46i67i433j0i20i263i433i512j46i433i512l2j69i60.1199j0j7&sourceid=chrome&ie=UTF-8
-    // There is much more to the IModel than what we completed in class
-    // the first casualty of war is the plan
     public interface IModel
     {
-        //ops
         /// <summary>
         /// Opens Cell
         /// </summary>
